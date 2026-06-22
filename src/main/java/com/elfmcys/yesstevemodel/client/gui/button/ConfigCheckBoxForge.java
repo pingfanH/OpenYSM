028a2b97ef07568ea32b53fb2bad9e9fae271111
pingfanH <pingfanh1145@gmail>
2026-06-22 23:16:57 +0800

    Fix: Checkbox constructor(+showLabel), AudioStreamCache, EffectLevel/EquippedEnchantmentLevel(Holder), textureManager
    - Remaining: renderEntityInInventory, renderNameTag, renderToBuffer, StateSwitchingButton, geckolib3 rendering

--- src/main/java/com/elfmcys/yesstevemodel/client/gui/button/ConfigCheckBoxForge.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/gui/button/ConfigCheckBoxForge.java
@@ -9,7 +9,7 @@ public class ConfigCheckBoxForge extends Checkbox {
     private final ModConfigSpec.BooleanValue forgeConfigSpec;
 
     public ConfigCheckBoxForge(int x, int y, String str, ModConfigSpec.BooleanValue booleanValue) {
-        super(x, y, 400, 20, Component.translatable("gui.yes_steve_model.config." + str), booleanValue.get().booleanValue());
+        super(x, y, 400, 20, Component.translatable("gui.yes_steve_model.config." + str), booleanValue.get().booleanValue(), false);
         this.forgeConfigSpec = booleanValue;
     }
 
